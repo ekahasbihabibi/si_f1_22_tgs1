c6d412055d3e05ca96c27713424b62d419728f06
ekahasbihabibi <ekahasbi01@gmail.com>
2025-05-10 23:38:43 +0700

    tgs-lanjutan web

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/si_f1_22';
+$config['base_url'] = 'http://localhost/si_f1_22_tgs1';
 
 /*
 |--------------------------------------------------------------------------
